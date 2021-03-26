59a13e9ae2066cf06067cce56f6925e86779036a
Haim Gelfenbeyn <haim@g8n.me>
2021-03-26 16:46:48 -0400

    Github workflow added

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 //! extern crate get_cookie;
 //!
 //! # fn main() {
-//!   let cookie = get_cookie(".mydomain.com", "my-cookie-name");
+//!   let cookie = get_cookie::get_cookie(".mydomain.com", "my-cookie-name");
 //! # }
 //! ```
 
@@ -14,7 +14,7 @@ mod chrome;
 mod cookie;
 mod errors;
 
-pub fn get_cookie(domain: &str, cookie: &str) -> Result<Option<cookie::Cookie>, GetCookieError> {
+pub fn get_cookie(domain: &str, cookie: &str) -> Result<Option<cookie::Cookie>, errors::GetCookieError> {
     // TODO: Add support for other browsers, check which browser has the latest cookie and return it.
     chrome::get_cookie(domain, cookie)
     //
